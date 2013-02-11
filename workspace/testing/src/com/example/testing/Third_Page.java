80d63cf5535e7e2e4eb5fc5a97cedadd3b02fd09
mschaddel <mschaddel@gmail.com>
2013-02-11 10:28:52 -0600

    Added third button

--- workspace/testing/src/com/example/testing/Third_Page.java
+++ workspace/testing/src/com/example/testing/Third_Page.java
@@ -1,24 +1,24 @@
 package com.example.testing;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
-
-public class Second_Page extends Activity{
-	
+public class Third_Page extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_page);
+        setContentView(R.layout.third_page);
         Intent i = getIntent();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.second_page, menu);
+        getMenuInflater().inflate(R.menu.third_page, menu);
         return true;
 			}
 	}
+
+
 
