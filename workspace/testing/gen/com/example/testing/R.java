9b54af14c5cdffb82fae8768c9b75dcc729777c4
Ryan McDole <rmcdole@slu.edu>
2013-02-17 18:22:14 -0600

    Added Login Page
    too be changed still

--- workspace/testing/gen/com/example/testing/R.java
+++ workspace/testing/gen/com/example/testing/R.java
@@ -11,22 +11,39 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int logo=0x7f020003;
+        public static final int repeat_bg=0x7f020004;
     }
     public static final class id {
-        public static final int Button_3=0x7f070002;
-        public static final int Calender_Button=0x7f070001;
-        public static final int GPS_Button=0x7f070003;
+        public static final int Button_3=0x7f070006;
+        public static final int Calender_Button=0x7f070005;
+        public static final int GPS_Button=0x7f070007;
+        public static final int btnLogin=0x7f070003;
+        public static final int btnRegister=0x7f07000b;
         public static final int edit_message=0x7f070000;
-        public static final int menu_settings=0x7f070005;
-        public static final int progressBar1=0x7f070004;
+        public static final int footer=0x7f070002;
+        public static final int header=0x7f070001;
+        public static final int link_to_login=0x7f07000c;
+        public static final int link_to_register=0x7f070004;
+        public static final int menu_settings=0x7f07000e;
+        public static final int progressBar1=0x7f07000d;
+        public static final int reg_email=0x7f070009;
+        public static final int reg_fullname=0x7f070008;
+        public static final int reg_password=0x7f07000a;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int new_file=0x7f030002;
-        public static final int second_page=0x7f030003;
-        public static final int third_page=0x7f030004;
+        public static final int footer_repeat=0x7f030002;
+        public static final int header_gradient=0x7f030003;
+        public static final int login=0x7f030004;
+        public static final int new_file=0x7f030005;
+        public static final int register=0x7f030006;
+        public static final int second_page=0x7f030007;
+        public static final int third_page=0x7f030008;
     }
     public static final class menu {
         public static final int activity_display_message=0x7f060000;
@@ -44,6 +61,8 @@ public final class R {
         public static final int button_send=0x7f040003;
         public static final int edit_message=0x7f040002;
         public static final int hello_world=0x7f040005;
+        public static final int link_to_login=0x7f04000d;
+        public static final int link_to_register=0x7f04000c;
         public static final int menu_settings=0x7f040001;
         public static final int title_activity_display_message=0x7f040007;
         public static final int title_activity_main=0x7f040004;
