4ad01724d913fbf2e5cb0ac6b43eadf35597b62e
Ryan McDole <rmcdole@slu.edu>
2013-02-22 10:25:14 -0600

    Changeed Logo

--- workspace/testing/gen/com/example/testing/R.java
+++ workspace/testing/gen/com/example/testing/R.java
@@ -18,28 +18,27 @@ public final class R {
         public static final int repeat_bg=0x7f020004;
     }
     public static final class id {
-        public static final int Button_3=0x7f07000f;
-        public static final int Calender_Button=0x7f07000e;
-        public static final int GPS_Button=0x7f070010;
+        public static final int Button_3=0x7f07000e;
+        public static final int Calender_Button=0x7f07000d;
+        public static final int GPS_Button=0x7f07000f;
         public static final int address=0x7f070005;
-        public static final int btnLogin=0x7f07000c;
-        public static final int btnRegister=0x7f070014;
+        public static final int btnLogin=0x7f07000b;
+        public static final int btnRegister=0x7f070013;
         public static final int distance=0x7f070009;
-        public static final int footer=0x7f07000b;
         public static final int header=0x7f07000a;
         public static final int label_address=0x7f070004;
         public static final int label_distance=0x7f070008;
         public static final int label_latlng=0x7f070002;
         public static final int latlng=0x7f070003;
-        public static final int link_to_login=0x7f070015;
-        public static final int link_to_register=0x7f07000d;
-        public static final int menu_settings=0x7f070017;
-        public static final int progressBar1=0x7f070016;
+        public static final int link_to_login=0x7f070014;
+        public static final int link_to_register=0x7f07000c;
+        public static final int menu_settings=0x7f070016;
+        public static final int progressBar1=0x7f070015;
         public static final int provider_both=0x7f070001;
         public static final int provider_fine=0x7f070000;
-        public static final int reg_email=0x7f070012;
-        public static final int reg_fullname=0x7f070011;
-        public static final int reg_password=0x7f070013;
+        public static final int reg_email=0x7f070011;
+        public static final int reg_fullname=0x7f070010;
+        public static final int reg_password=0x7f070012;
         public static final int start_run=0x7f070006;
         public static final int stop_run=0x7f070007;
     }
